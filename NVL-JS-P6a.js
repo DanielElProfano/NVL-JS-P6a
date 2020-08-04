2e2a153d94a0aa51067c681be4236c7ea84a0001
Daniel Gonzalez <daniel.gonzalez.guijarro@gmail.com>
2020-08-04 23:44:13 +0200

    PRACTICA CON VARIABLE

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,5 @@
+var nombre_completo = 'Daniel Gonzalez Guijarro';
+var edad = 18;
+var dia1 = ['lunes','martes','miercoles','jueves','viernes','sabado','domingo'];
+var gravedaad = 9.8;
+var impares = [1,3,5];
